cb8090c73bed708f6835b9f52341a6fe1d904bd1
unknown <unknown@example.com>
2019-06-24 15:43:18 +0800

    故事内容添加
    故事内容添加，顶部分类栏功能添加，内容部分添加

--- pages/stories/stories.js
+++ pages/stories/stories.js
@@ -4,7 +4,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    categoryDatas:[
+      {
+        cateId:"001",
+        cateName:"所有故事"
+      },
+      {
+        cateId: "002",
+        cateName: "民宿"
+      },
+      {
+        cateId: "003",
+        cateName: "美食"
+      },
+      {
+        cateId: "004",
+        cateName: "景点"
+      },
+      {
+        cateId: "005",
+        cateName: "艺术"
+      },
+      {
+        cateId: "006",
+        cateName: "灵感"
+      },
+      {
+        cateId: "007",
+        cateName: "娱乐"
+      },
+    ],
+    currentCate:0,
+    storyDatas:[
+      {
+        storyName:"咖啡店",
+        storyContent:[
+          {
+            loc:"曼谷",
+            des:"泰国时髦星人喜欢的Roast，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "成都",
+            des: "一场关于自然的巡演，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "上海",
+            des: "世界规模最大的模型展，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "佛罗伦萨",
+            des: "学会享受生活，你连一次基本的野餐都组织不起来"
+          },
+        ]
+      },
+      {
+        storyName: "餐厅",
+        storyContent: [
+          {
+            loc: "曼谷",
+            des: "泰国时髦星人喜欢的Roast，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "成都",
+            des: "一场关于自然的巡演，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "上海",
+            des: "世界规模最大的模型展，你连一次基本的野餐都组织不起来"
+          },
+          {
+            loc: "佛罗伦萨",
+            des: "学会享受生活，你连一次基本的野餐都组织不起来"
+          },
+        ]
+      },
+    ]
+
+  },
+  onCateSelect:function (event) {
+    this.setData({
+      currentCate: event.currentTarget.dataset['index']
+    })
   },
 
   /**
